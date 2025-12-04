eb604423f8932048738cedaa183e55d3e4fa3ae2
Ryan Moses <codekeys.moses.dev@gmail.com>
2025-12-04 00:08:06 -0500

    GUSA-33 [FEATURE]: produce child components of ProfileCard

--- src/components/ProfileCard/CardBody.jsx
+++ src/components/ProfileCard/CardBody.jsx
@@ -1,7 +1,26 @@
+import clsx from "clsx";
 import styles from "./ProfileCard.module.css";
 
 const CardBody = () => {
-  return <div>CardBody</div>;
+  return (
+    <div className={styles["card-body-container"]}>
+      <p className={clsx("txt-preset-6", styles.bio)}>This profile has no bio</p>
+      <div className={styles["profile-info"]}>
+        <div>
+          <h6 className='txt-preset-7'>Repos</h6>
+          <p className='txt-preset-2'>8</p>
+        </div>
+        <div>
+          <h6 className='txt-preset-7'>Followers</h6>
+          <p className='txt-preset-2'>3938</p>
+        </div>
+        <div>
+          <h6 className='txt-preset-7'>Following</h6>
+          <p className='txt-preset-2'>9</p>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default CardBody;
--- src/components/ProfileCard/CardFooter.jsx
+++ src/components/ProfileCard/CardFooter.jsx
@@ -1,7 +1,30 @@
 import styles from "./ProfileCard.module.css";
+import locationIcon from "@assets/location-icon.svg";
+import twitterLogo from "@assets/twitter-icon.svg";
+import blogLogo from "@assets/blog-icon.svg";
+import companyIcon from "@assets/company-icon.svg";
 
 const CardFooter = () => {
-  return <div>CardFooter</div>;
+  return (
+    <div className={styles["card-footer-container"]}>
+      <div>
+        <img src={locationIcon} alt='location icon' />
+        <p className="txt-preset-6">San Francisco</p>
+      </div>
+      <div>
+        <img src={twitterLogo} alt='twitter logo' />
+        <p className="txt-preset-6">Not Available</p>
+      </div>
+      <div>
+        <img src={blogLogo} alt='blog logo' />
+        <p className="txt-preset-6">https://github.blog</p>
+      </div>
+      <div>
+        <img src={companyIcon} alt='company icon' />
+        <p className="txt-preset-6">@github</p>
+      </div>
+    </div>
+  );
 };
 
 export default CardFooter;
